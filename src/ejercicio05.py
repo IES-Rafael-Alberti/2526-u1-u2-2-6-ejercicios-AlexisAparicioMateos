846255c656db56eadc3ad9927429db4345d127db
AlexisAparicioMateos <aapariciom@iesrafaelalberti.es>
2025-10-29 18:30:56 +0100

    Ejercicio05 terminado

--- src/ejercicio05.py
+++ src/ejercicio05.py
@@ -28,9 +28,19 @@ def convertir_segundos(segundos_totales: int) -> tuple[int, int, int, int]:
         - 1 hora = 3600 segundos
         - 1 minuto = 60 segundos
     """
-    # TODO: Implementar la función
-    return (0, 0, 0, 0)
+    # IMPLEMENTACIÓN DE LA FUNCIÓN
 
+    if segundos_totales < 0:
+        return(0, 0, 0, 0)
+
+    dias = segundos_totales // 86400
+    resto = segundos_totales % 86400
+    horas = resto // 3600
+    resto = resto % 3600
+    minutos = resto // 60
+    segundos = resto % 60
+
+    return (dias, horas, minutos, segundos)
 
 def solicitar_segundos() -> int:
     """
